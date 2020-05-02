7c4b00b0c6070e8bf15cb51342da2a0b41145105
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2020-05-02 10:14:13 +0300

    Add credentials

---
+++ conf/secrets/habitica_credentials_template.py
@@ -0,0 +1,15 @@
+"""
+Keep the contents of this file private! User ID is public information, but API
+token is as powerful as your password.
+
+After editing this file as instructed below, save it as "secrets.py".
+"""
+
+# Replace yourUID with your user ID (keep the quotes). If you need help
+# determining it, see
+# https://habitica.fandom.com/wiki/API_Options#User_ID_.28UID.29
+PLAYER_USER_ID = "yourUID"
+
+# Replace yourToken with your API token (keep the quotes). For help, see
+# https://habitica.fandom.com/wiki/API_Options#API_Token
+PLAYER_API_TOKEN = "yourToken"
