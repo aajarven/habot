422f9cc512882b0100c361732064ef2681a41b08
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2020-05-19 16:25:34 +0000

    Get started with reading the weekly questions from a file.

--- habot/io.py
+++ habot/io.py
@@ -43,3 +43,32 @@ class HabiticaMessager(object):
             raise CommunicationFailedException(response)
 
         self._habitica_operator.tick_task(PM_SENT, task_type="habit")
+
+
+class YamlFileIO(object):
+    """
+    Read and write YAML files.
+    """
+
+    @classmethod
+    def read_question_list(cls, filename, unused_only=True):
+        """
+        Parse a YAML file containing weekly questions.
+
+        The expected syntax for the file is that it contains a list
+        "questions", each item of which is a dict with keys "question",
+        "description" and "used". For example
+
+        questions:
+          - question: What is your favourite fruit?
+            description: Do you like bananas or apples more? Or maybe kiwis?
+            used: True
+          - question: What is your favourite animal?
+            description: The only real answer here is labrador though =3
+            used: False
+
+        is a valid question list file.
+
+        :returns: a list of Tasks
+        """
+        raise NotImplementedError("YAML file IO not implemented yet.")
