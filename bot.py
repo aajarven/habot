928b3976550dfacb25694458ff86c3bb52439991
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2020-06-03 15:44:39 +0000

    Add DB sync to birthday reminding

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 CLI for performing bot actions
 """
 
+import traceback
+
 import datetime
 import click
 
@@ -11,7 +13,7 @@ from habitica_helper.challenge import Challenge
 from conf.header import HEADER, PARTYMEMBER_HEADER
 from conf.tasks import WINNER_PICKED
 from habot.birthdays import BirthdayReminder
-from habot.io import HabiticaMessager
+from habot.io import HabiticaMessager, DBSyncer
 from habot.habitica_operations import HabiticaOperator
 from habot.sharing_weekend import SharingChallengeOperator
 
@@ -132,10 +134,18 @@ def send_pm(message, recipient_uid):
               default="f687a6c7-860a-4c7c-8a07-9d0dcbb7c831",
               help=("Habitica user ID of the recipient. Default "
                     "is Antonbury's"))
-def send_birthday_reminder(recipient_uid):
+@click.option("--no-sync", is_flag=True,
+              help=("Don't update the database using the fresh data from the "
+                    "API, but use old database data instead. This makes the "
+                    "command run somewhat faster but risks using outdated "
+                    "data."))
+def send_birthday_reminder(recipient_uid, no_sync):
     """
     Send a private message listing everyone who is having their birthday.
     """
+    if not no_sync:
+        db_syncer = DBSyncer(PARTYMEMBER_HEADER)
+        db_syncer.update_partymember_data()
     reminder = BirthdayReminder(HEADER)
     reminder.send_birthday_reminder(recipient_uid)
 
