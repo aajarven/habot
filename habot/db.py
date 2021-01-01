7edc697256cdc0791eca91701dac51424ae5b519
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-01-01 17:58:51 +0000

    Make linter not fail on missing secrets, as they are added by the user

--- habot/db.py
+++ habot/db.py
@@ -5,7 +5,12 @@ Database operations for the bot.
 import mysql.connector
 
 import conf.db as dbconf
+
+# credentials added by user, not present always when linting
+# pylint: disable=no-name-in-module,import-error
 import conf.secrets.db_credentials as credentials
+# pylint: enable=no-name-in-module,import-error
+
 import habot.logger
 
 
