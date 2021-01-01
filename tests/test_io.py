8bc11ffbbf26ca2c3b9241e11e3bcff9c9cfc3eb
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-01-01 13:20:38 +0000

    Add tests for reading chat and system messages

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,9 +2,11 @@
 Test `habot.io` module.
 """
 
+import datetime
 import pytest
 from unittest import mock
 
+from habot.db import DBOperator
 from habot.io import HabiticaMessager
 
 
@@ -31,3 +33,125 @@ def test_group_message(mock_tick, mock_post, test_messager, header_fx):
             headers=header_fx,
             data={"message": "some message"})
     mock_tick.assert_called()
+
+
+@pytest.fixture
+def db_operator_fx(db_connection_fx):
+    """
+    Yield an operator for a test database.
+    """
+    yield DBOperator()
+
+
+USER_MESSAGE_1 = {
+        "flagCount": 0,
+        "_id": "unique-message-id",
+        "id": "unique-message-id",
+        "user": "MessageSenderDisplayname",
+        "username": "MessageSender Displayname",
+        "uuid": "MessageSenderUserID",
+        "groupId": "group-id",
+        "text": "Some test message",
+        "timestamp": 1609490392299,
+        "likes": {},
+        "flags": {},
+}
+
+USER_MESSAGE_2 = {
+        "flagCount": 0,
+        "_id": "different-id",
+        "id": "different-id",
+        "user": "AnotherSender",
+        "username": "Another Sender",
+        "uuid": "AnotherMessageSenderUserID",
+        "groupId": "group-id",
+        "text": ("I have a lot to say, all kinds of important stuff.\n\n"
+                 "Many rows!"),
+        "timestamp": 1609480474296,
+        "likes": {"MessageSenderUserID": True},
+        "flags": {},
+}
+
+SYSTEM_MESSAGE = {
+    "flagCount": 0,
+    "_id": "unique-system-message-id",
+    "flags": {},
+    "id": "unique-system-message-id",
+    "text": "`Some Guy casts Earthquake for the party.`",
+    "unformattedText": "Some Guy casts Earthquake for the party.",
+    "info": {"type": "spell_cast_party",
+             "user": "SomeGuy",
+             "class": "wizard",
+             "spell": "earth"},
+    "timestamp": 1609486163423,
+    "likes": {},
+    "uuid": "system",
+    "groupId": "group-id"
+    }
+
+
+@pytest.fixture
+def patch_chat_api_response(monkeypatch):
+    """
+    Allow monkeypatching `get_dict_from_api` to return arbitrary data.
+    """
+    def _patch(messages):
+        def _return_messages(*args, **kwargs):
+            return messages
+        monkeypatch.setattr("habot.io.get_dict_from_api",
+                            _return_messages)
+    return _patch
+
+
+def test_get_party_messages(test_messager, db_operator_fx,
+                            patch_chat_api_response):
+    """
+    Test that correct data is written to the database for messages.
+
+    A single chat and system messages are tested.
+    """
+    patch_chat_api_response([USER_MESSAGE_1, SYSTEM_MESSAGE])
+    test_messager.get_party_messages()
+    chat_messages = db_operator_fx.query_table("chat_messages")
+    system_messages = db_operator_fx.query_table("system_messages")
+    assert len(chat_messages) == 1
+    assert len(system_messages) == 1
+
+    expected_chat_message = {
+        "id": USER_MESSAGE_1["id"],
+        "from_id": USER_MESSAGE_1["uuid"],
+        "to_group": USER_MESSAGE_1["groupId"],
+        "timestamp": datetime.datetime(2021, 1, 1, 8, 39, 52),
+        "content": USER_MESSAGE_1["text"],
+        }
+    for key in expected_chat_message:
+        assert chat_messages[0][key] == expected_chat_message[key]
+
+    expected_system_message = {
+        "id": SYSTEM_MESSAGE["id"],
+        "to_group": SYSTEM_MESSAGE["groupId"],
+        "timestamp": datetime.datetime(2021, 1, 1, 7, 29, 23),
+        "content": SYSTEM_MESSAGE["text"],
+        }
+    for key in expected_system_message:
+        assert system_messages[0][key] == expected_system_message[key]
+
+
+def test_fetch_two_chat_messages(test_messager, db_operator_fx,
+                                 patch_chat_api_response):
+    """
+    Ensure that when new two messages are read, two messages end up in the db.
+    """
+    patch_chat_api_response([USER_MESSAGE_1, USER_MESSAGE_2])
+    test_messager.get_party_messages()
+    assert len(db_operator_fx.query_table("chat_messages")) == 2
+
+
+def test_old_messages(test_messager, db_operator_fx, patch_chat_api_response):
+    """
+    Read the same messages twice and make sure db only has one copy of each.
+    """
+    patch_chat_api_response([USER_MESSAGE_1, USER_MESSAGE_2])
+    test_messager.get_party_messages()
+    test_messager.get_party_messages()
+    assert len(db_operator_fx.query_table("chat_messages")) == 2
