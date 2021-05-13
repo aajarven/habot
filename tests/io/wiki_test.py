132bd79928c836478b9ec1945e1de565a4e5e353
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-05-13 15:11:20 +0000

    Make it possible to convert HTML to markdown
    The content read from wiki is HTML, but Habitica requires party
    descriptions etc. to be written in markdown. Add support for converting
    the following HTML elements to markdown:
        - <i> (italic)
        - <b> (bold)
        - <s> (strikethrough)
        - <code> (inline code)
        - <ol> and the <li> within (ordered list)
        - <ul> and the <li> within (unordered list)

--- tests/io/wiki_test.py
+++ tests/io/wiki_test.py
@@ -2,9 +2,10 @@
 Test `habot.io.wiki` module.
 """
 
+from lxml import etree
 import pytest
 
-from habot.io.wiki import WikiReader
+from habot.io.wiki import WikiReader, HtmlToMd
 
 
 @pytest.fixture
@@ -61,3 +62,68 @@ def test_find_elements_with_matching_subelement():
 
     for element in li_elements:
         assert element.tag == "li"
+
+
+@pytest.fixture
+def complex_test_tree():
+    """
+    Return a lxml etree containing three levels of elements.
+
+    The HTML to which the tree correspondings is the following:
+    ```
+    <ol>
+      <li>There is something in italic <i>here</i>, how cool</li>
+      <li>There is something bold <b>here</b>, how cool</li>
+      <li>There is something <s>strikethroughed</s> here, how cool</li>
+      <li>There is some <code>code</code> here, how cool</li>
+    </ol>
+    ```
+    """
+    html_str = (
+            "<ol>"
+            "<li>There is something in italic <i>here</i>, how cool</li>"
+            "<li>There is something bold <b>here</b>, how cool</li>"
+            "<li>There is something <s>strikethroughed</s> here, how cool</li>"
+            "<li>There is some <code>code</code> here, how cool</li>"
+            "</ol>"
+            )
+    root = etree.fromstring(html_str)
+    return root
+
+
+def test_depth_first_traversal_order(complex_test_tree):
+    """
+    Ensure that the tree is traversed in post-order.
+
+    The nodes should be yielded so that a formatting nodes (`i`, `b`, `code`
+    etc) and `li` nodes alternate, and each formatting node is returned just
+    before its parent list item node.
+    """
+    # We are testing a private method here
+    # pylint: disable=protected-access
+
+    html2md = HtmlToMd()
+    nodes_in_order = list(html2md._traverse_depth_first(complex_test_tree))
+
+    for i in range(0, len(nodes_in_order)-1, 2):
+        assert nodes_in_order[i].getparent() == nodes_in_order[i+1]
+
+
+def test_convert(complex_test_tree):
+    """
+    Test that the following elements are converted to markdown correctly:
+    - <i> (italic)
+    - <b> (bold)
+    - <s> (strikethrough)
+    - <code> (inline code)
+    - <ol> and the <li> within
+    """
+    html2md = HtmlToMd()
+    converted = html2md.convert(complex_test_tree)
+
+    expected_result = ("1. There is something in italic *here*, how cool\n"
+                       "1. There is something bold **here**, how cool\n"
+                       "1. There is something ~~strikethroughed~~ here, "
+                       "how cool\n"
+                       "1. There is some `code` here, how cool\n\n")
+    assert converted == expected_result
